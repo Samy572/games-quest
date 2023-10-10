da481883c64104f04e20f63071e8e31001751743
Sam <samy_ch@outlook.fr>
2023-10-10 22:36:19 +0200

    ✨ Add ui component from shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 'use client';
-import { Button } from '@/components/ui/button';
 
 export default function Home() {
 	return (
 		<div>
-			<Button variant={'ghost'}> Click me </Button>
+			
 		</div>
 	);
 }
