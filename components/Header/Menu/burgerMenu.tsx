dbf9f3a537bd3b2e2ee8895f1560e046af197aa9
Sam <samy_ch@outlook.fr>
2023-10-11 17:07:23 +0200

    ✨ Create Context

---
+++ components/Header/Menu/burgerMenu.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import Context from '@/context/Context';
+import { Menu, X } from 'lucide-react';
+import { useContext, useState } from 'react';
+export const BurgerMenu = () => {
+	const { toggleMenu, toggleMenuHandler } = useContext(Context);
+
+	return (
+		<div className="cursor-pointer">
+			{toggleMenu === false ? (
+				<Button
+					onClick={() => toggleMenuHandler()}
+					variant={'default'}
+					aria-label="navigation"
+					className="hover:bg-neutral-800 rounded-full"
+				>
+					<Menu />
+				</Button>
+			) : (
+				<Button
+					onClick={() => toggleMenuHandler()}
+					variant={'default'}
+					aria-label="navigation"
+					className="hover:bg-neutral-800 rounded-full"
+				>
+					<X />
+				</Button>
+			)}
+		</div>
+	);
+};
