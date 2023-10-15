fa4e98b69bbc9ad61433f6736c716c981c79058c
Sam <samy_ch@outlook.fr>
2023-10-15 12:37:48 +0200

    💄 Update searchlist image

--- components/searchlist.tsx
+++ components/searchlist.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 const SearchList = ({
 	name,
 	children,
-	background_image
+	background_image,
 }: {
 	children?: React.ReactNode;
 	name: string;
@@ -11,20 +11,20 @@ const SearchList = ({
 }) => {
 	return (
 		<div className="flex items-center p-[2px] py-2 hover:bg-white select-none px-3">
-		<p>{name}</p>
-		{background_image && (
-			<div>
-				<Image
-					className="mr-2"
-					src={background_image}
-					alt={name}
-					width={50}
-					height={50}
-				/>
-			</div>
-		)}
-		<div>{children}</div>
-	</div>
-);
+			{background_image && (
+				<div>
+					<Image
+						className="mr-2"
+						src={background_image}
+						alt={name}
+						width={50}
+						height={50}
+					/>
+				</div>
+			)}
+			<p>{name}</p>
+			<div>{children}</div>
+		</div>
+	);
 };
 export default SearchList;
