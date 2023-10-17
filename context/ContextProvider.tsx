88b459392fa51b4eb3686dde9321599e8550315d
Sam <samy_ch@outlook.fr>
2023-10-17 17:33:52 +0200

    ♻️ Refacto context Provider remove switch case foreach url

--- context/ContextProvider.tsx
+++ context/ContextProvider.tsx
@@ -14,13 +14,14 @@ export default function ContextProvider({
 	const [dataSearchInput, setDataSearchInput] = useState([]);
 	const APIKEY = process.env.SECRET;
 
-	const url2023 = `https://api.rawg.io/api/games?page_size=18&key=${APIKEY}&dates=2023-01-01,2023-12-31&ordering=-added`;
-	const url2022 = `https://api.rawg.io/api/games?page_size=18&key=${APIKEY}&dates=2022-01-01,2022-12-31&ordering=-added`;
-	const url2021 = `https://api.rawg.io/api/games?page_size=18&key=${APIKEY}&dates=2021-01-01,2021-12-31&ordering=-added`;
+	const baseApiUrl = 'https://api.rawg.io/api/games';
+	const defaultUrlParams = `?page_size=18&key=${APIKEY}&ordering=-added`;
 	const searchGames = `https://api.rawg.io/api/games?key=${APIKEY}&search=${inputValue}`;
 
-	// const urlByplatform = `https://api.rawg.io/api/games?key=${APIKEY}&platforms=${plaformValue}&page_size=18`;
-	const [url, setUrl] = useState(`${url2023}`);
+	const [url, setUrl] = useState(
+		`${baseApiUrl}${defaultUrlParams}&dates=2023-01-01,2023-12-31`
+	);
+
 	const toggleMenuHandler = () => {
 		setToggleMenu(!toggleMenu);
 	};
@@ -29,7 +30,8 @@ export default function ContextProvider({
 		setInputValue(e.target.value);
 		console.log(e);
 	};
-	// Fetch data from API
+
+	// Fetch data from API to display main data
 	useEffect(() => {
 		async function fetchData() {
 			try {
@@ -46,24 +48,14 @@ export default function ContextProvider({
 		}
 		fetchData();
 	}, [url]);
-	// Display the game according to the URL
-	const urlHandler = (url: string) => {
-		switch (url) {
-			case 'url2023':
-				setUrl(url2023);
-				break;
-			case 'url2022':
-				setUrl(url2022);
-				break;
-			case 'url2021':
-				setUrl(url2021);
-				break;
 
-			default:
-				setUrl(url2023);
-				break;
-		}
+	const urlHandler = (year: string) => {
+		const selectedYear = year.includes(year) ? year : '2023';
+		setUrl(
+			`${baseApiUrl}${defaultUrlParams}&dates=${selectedYear}-01-01,${selectedYear}-12-31`
+		);
 	};
+
 	// Search game with input
 	useEffect(() => {
 		if (inputValue === '') {
@@ -74,13 +66,11 @@ export default function ContextProvider({
 			try {
 				const response = await fetch(searchGames);
 				const data = await response.json();
-				console.log(data);
 				setDataSearchInput(data.results);
 			} catch (error) {
 				console.error('error :', error);
 			}
 		};
-
 		fetchGames();
 	}, [inputValue, searchGames]);
 
@@ -105,7 +95,3 @@ export default function ContextProvider({
 
 	return <Context.Provider value={contextValue}>{children}</Context.Provider>;
 }
-
-// PC = 4
-// PS5 = 187
-// XBOX serie s = 186
