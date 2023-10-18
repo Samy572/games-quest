98c843e2740afe95bfde61eacc0e4343949dcb5c
Sam <samy_ch@outlook.fr>
2023-10-18 11:44:01 +0200

    🔍 Change main location to /home

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
 	env: {
 		SECRET: '22dc8451931443c6bc3fbc279f7a8c7b',
 	},
+	async rewrites() {
+		return [
+			{
+				source: '/home',
+				destination: '/',
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
