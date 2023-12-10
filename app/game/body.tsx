258d5b0b3432dce52c7da82177b1f385d94bd055
Sam <samy_ch@outlook.fr>
2023-12-10 17:41:01 +0100

    ♻️ ⚡️ refactor page game to retrieve selected game

--- app/game/body.tsx
+++ app/game/body.tsx
@@ -7,30 +7,37 @@ import Context from '@/context/Context';
 import { useContext } from 'react';
 
 const Body = ({ selectedGameData }: { selectedGameData: GameCardType }) => {
-	const { handleAddFavoris, listFavorite } = useContext(Context);
+	// const { handleAddFavoris, listFavorite } = useContext(Context);
+	console.log('selectedGameData :', selectedGameData);
 	return (
 		<div className="text-left pt-5 text-4xl">
 			<h2 className="pb-5 text-primary font-semibold">
 				{selectedGameData.name}
 			</h2>
 			<div className="flex  items-center">
-				<span className=" text-xl">Released: {selectedGameData.released}</span>
+				<span className=" text-xl">
+					Released: {selectedGameData && selectedGameData.released}
+				</span>
 			</div>
 			<div className="flex justify-between">
 				<div className="flex items-center gap-1">
-					{selectedGameData?.genres.slice(0, 2).map((genre) => (
-						<Badge
-							variant={'secondary'}
-							key={genre.id}
-							className="w-fit text-primary"
-						>
-							{genre.name}
-						</Badge>
-					))}
+					{selectedGameData &&
+						selectedGameData.genres.slice(0, 2).map((genre) => (
+							<Badge
+								variant={'secondary'}
+								key={genre.id}
+								className="w-fit text-primary"
+							>
+								{genre.name}
+							</Badge>
+						))}
 				</div>
-				<Displayplatforms platforms={selectedGameData.platforms} img="../img" />
+				<Displayplatforms
+					platforms={selectedGameData && selectedGameData.platforms}
+					img="../img"
+				/>
 			</div>
-			<Favoris
+			{/* <Favoris
 				onClick={() => handleAddFavoris(selectedGameData)}
 				description={
 					listFavorite.some((item: any) => item.id === selectedGameData.id)
@@ -42,7 +49,7 @@ const Body = ({ selectedGameData }: { selectedGameData: GameCardType }) => {
 						? '../img/Star_full.svg'
 						: '../img/Star_empty.svg'
 				}
-			/>
+			/> */}
 		</div>
 	);
 };
