cf9887d6484d985253605879ced274a99434ada6
Sam <samy_ch@outlook.fr>
2024-01-13 15:14:22 +0100

    🐛 Fix display bug

--- app/game/body.tsx
+++ app/game/body.tsx
@@ -7,6 +7,7 @@ import useFavoris from '@/hooks/useFavoris';
 import { Button } from '@/src/components/ui/button';
 import Link from 'next/link';
 import { ArrowUpRight } from 'lucide-react';
+import Rating from '@/src/components/rating';
 
 const Body = ({ selectedGameData }: { selectedGameData: GameCardType }) => {
 	const { handleAddFavoris, listFavorite } = useFavoris();
@@ -37,41 +38,40 @@ const Body = ({ selectedGameData }: { selectedGameData: GameCardType }) => {
 				)}
 			</div>
 			<div className="flex  items-center">
-				<span className=" text-xl">
-					Released: {selectedGameData && selectedGameData.released}
-				</span>
+				<span className=" text-xl">Released: {selectedGameData.released}</span>
 			</div>
 			<div className="flex justify-between">
 				<div className="flex items-center gap-1">
 					{selectedGameData &&
 						selectedGameData.genres.slice(0, 2).map((genre) => (
-							<Badge
-								variant={'secondary'}
-								key={genre.id}
-								className="w-fit text-primary"
-							>
+							<Badge variant={'default'} key={genre.id} className="w-fit ">
 								{genre.name}
 							</Badge>
 						))}
 				</div>
-				<Displayplatforms
-					platforms={selectedGameData && selectedGameData.platforms}
-					img="../img"
-				/>
+				<Displayplatforms platforms={selectedGameData.platforms} img="../img" />
 			</div>
-			<Favoris
-				onClick={() => handleAddFavoris(selectedGameData)}
-				description={
-					listFavorite.some((item: any) => item.id === selectedGameData.id)
-						? 'Remove from favoris'
-						: 'Add to favoris'
-				}
-				img={
-					listFavorite.some((item: any) => item.id === selectedGameData.id)
-						? '../img/Star_full.svg'
-						: '../img/Star_empty.svg'
-				}
-			/>
+			<div className="flex justify-between items-center">
+				<Favoris
+					onClick={() => handleAddFavoris(selectedGameData)}
+					description={
+						listFavorite.some((item: any) => item.id === selectedGameData.id)
+							? 'Remove from favoris'
+							: 'Add to favoris'
+					}
+					img={
+						listFavorite.some((item: any) => item.id === selectedGameData.id)
+							? '../img/Star_full.svg'
+							: '../img/Star_empty.svg'
+					}
+				/>
+
+				<div className=" text-xl text-primary/80 p-2 flex items-center gap-x-4">
+					<span className="text-foreground"> Score</span>
+
+					<Rating selectedGameData={selectedGameData} />
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/components/navmenu.tsx
+++ src/components/navmenu.tsx
@@ -3,21 +3,23 @@ import Link from 'next/link';
 
 export const Navmenu = () => {
 	return (
-		<div>
-			<ul className="lg:hidden h-12 text-xs  border-t border-slate-400 bg-black text-primary fixed bottom-0 left-0 right-0 flex items-center justify-evenly opacity-90 ">
-				<Link href={'/home'} className="flex items-center flex-col">
-					<Home size={20} /> Home
+		<ul
+			className={
+				' lg:hidden h-12 text-xs  border-t border-slate-400 bg-black text-primary fixed bottom-0 left-0 right-0 flex items-center justify-evenly opacity-90 '
+			}
+		>
+			<Link href={'/home'} className="flex items-center flex-col">
+				<Home size={20} /> Home
+			</Link>
+			<Link href={'/search'} className="flex items-center flex-col">
+				<Search size={20} /> Search
+			</Link>
+			<li>
+				<Link href={'/mylist'} className="flex items-center flex-col">
+					<Star size={20} />
+					My List
 				</Link>
-				<Link href={'/search'} className="flex items-center flex-col">
-					<Search size={20} /> Search
-				</Link>
-				<li>
-					<Link href={'/mylist'} className="flex items-center flex-col">
-						<Star size={20} />
-						My List
-					</Link>
-				</li>
-			</ul>
-		</div>
+			</li>
+		</ul>
 	);
 };
