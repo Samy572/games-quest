b5b19ce48d3ae5f0cf1b544991136126762949f0
Sam <samy_ch@outlook.fr>
2024-01-26 14:51:30 +0100

    🐛 Remove warning and msg error

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body
 				suppressHydrationWarning={true}
 				className={clsx(monstserrat.className)}
